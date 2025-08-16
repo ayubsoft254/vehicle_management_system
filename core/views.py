7ddc12e47e6f80b9ef9d93d7a56d91016af4126b
ayubsoft254 <henryoayub15@gmail.com>
2025-08-16 09:34:33 +0300

    feat: Add dashboard and landing page views with templates for user navigation

--- core/views.py
+++ core/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import User, RolePermission, Company
+
+# Dashboard view
+@method_decorator(login_required, name='dispatch')
+class DashboardView(TemplateView):
+	template_name = 'dashboard/dashboard.html'
+
+# Landing page view
+class LandingPageView(TemplateView):
+	template_name = 'dashboard/landing.html'
+
+# User list view
+@method_decorator(login_required, name='dispatch')
+class UserListView(ListView):
+	model = User
+	template_name = 'dashboard/user_list.html'
+	context_object_name = 'users'
+
+# User detail view
+@method_decorator(login_required, name='dispatch')
+class UserDetailView(DetailView):
+	model = User
+	template_name = 'dashboard/user_detail.html'
+	context_object_name = 'user_obj'
+
+# Company list view
+@method_decorator(login_required, name='dispatch')
+class CompanyListView(ListView):
+	model = Company
+	template_name = 'dashboard/company_list.html'
+	context_object_name = 'companies'
+
+# Company detail view
+@method_decorator(login_required, name='dispatch')
+class CompanyDetailView(DetailView):
+	model = Company
+	template_name = 'dashboard/company_detail.html'
+	context_object_name = 'company'
