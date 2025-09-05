d93fa0f9a933de5ff09ab6ac78717c0f2bc2f6c0
ayubsoft254 <henryoayub15@gmail.com>
2025-09-05 08:47:18 +0300

    feat: Add views and URLs for report templates and report generations management

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView
+from .models import ReportTemplate, ReportGeneration
+from django.urls import reverse_lazy
+
+class ReportTemplateListView(ListView):
+	model = ReportTemplate
+	template_name = 'reports/reporttemplate_list.html'
+	context_object_name = 'templates'
+
+class ReportTemplateDetailView(DetailView):
+	model = ReportTemplate
+	template_name = 'reports/reporttemplate_detail.html'
+	context_object_name = 'template'
+
+class ReportTemplateCreateView(CreateView):
+	model = ReportTemplate
+	fields = '__all__'
+	template_name = 'reports/reporttemplate_form.html'
+	success_url = reverse_lazy('reports:reporttemplate-list')
+
+class ReportGenerationListView(ListView):
+	model = ReportGeneration
+	template_name = 'reports/reportgeneration_list.html'
+	context_object_name = 'generations'
+
+class ReportGenerationDetailView(DetailView):
+	model = ReportGeneration
+	template_name = 'reports/reportgeneration_detail.html'
+	context_object_name = 'generation'
+
+class ReportGenerationCreateView(CreateView):
+	model = ReportGeneration
+	fields = '__all__'
+	template_name = 'reports/reportgeneration_form.html'
+	success_url = reverse_lazy('reports:reportgeneration-list')
