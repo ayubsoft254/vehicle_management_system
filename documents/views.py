53319480978d7ef232037ff0100838a7c9f9c060
ayubsoft254 <henryoayub15@gmail.com>
2025-09-05 08:39:46 +0300

    feat: Add views and URLs for document and document category management

--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Document, DocumentCategory
+from django.urls import reverse_lazy
+
+class DocumentListView(ListView):
+	model = Document
+	template_name = 'documents/document_list.html'
+	context_object_name = 'documents'
+
+class DocumentDetailView(DetailView):
+	model = Document
+	template_name = 'documents/document_detail.html'
+	context_object_name = 'document'
+
+class DocumentCreateView(CreateView):
+	model = Document
+	fields = '__all__'
+	template_name = 'documents/document_form.html'
+	success_url = reverse_lazy('documents:document-list')
+
+class DocumentCategoryListView(ListView):
+	model = DocumentCategory
+	template_name = 'documents/documentcategory_list.html'
+	context_object_name = 'categories'
+
+class DocumentCategoryDetailView(DetailView):
+	model = DocumentCategory
+	template_name = 'documents/documentcategory_detail.html'
+	context_object_name = 'category'
+
+class DocumentCategoryCreateView(CreateView):
+	model = DocumentCategory
+	fields = '__all__'
+	template_name = 'documents/documentcategory_form.html'
+	success_url = reverse_lazy('documents:documentcategory-list')
