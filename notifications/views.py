bd352cd23247a54b7bed4cb191c57ecfb4a811bc
ayubsoft254 <henryoayub15@gmail.com>
2025-09-05 08:44:56 +0300

    feat: Add views and URLs for notification and notification template management

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Notification, NotificationTemplate
+from django.urls import reverse_lazy
+
+class NotificationListView(ListView):
+	model = Notification
+	template_name = 'notifications/notification_list.html'
+	context_object_name = 'notifications'
+
+class NotificationDetailView(DetailView):
+	model = Notification
+	template_name = 'notifications/notification_detail.html'
+	context_object_name = 'notification'
+
+class NotificationCreateView(CreateView):
+	model = Notification
+	fields = '__all__'
+	template_name = 'notifications/notification_form.html'
+	success_url = reverse_lazy('notifications:notification-list')
+
+class NotificationTemplateListView(ListView):
+	model = NotificationTemplate
+	template_name = 'notifications/notificationtemplate_list.html'
+	context_object_name = 'templates'
+
+class NotificationTemplateDetailView(DetailView):
+	model = NotificationTemplate
+	template_name = 'notifications/notificationtemplate_detail.html'
+	context_object_name = 'template'
+
+class NotificationTemplateCreateView(CreateView):
+	model = NotificationTemplate
+	fields = '__all__'
+	template_name = 'notifications/notificationtemplate_form.html'
+	success_url = reverse_lazy('notifications:notificationtemplate-list')
