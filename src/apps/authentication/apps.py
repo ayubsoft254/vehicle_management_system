639b6009016208affe4686692489607a50d39845
ayubsoft254 <henryoayub15@gmail.com>
2025-10-12 14:40:31 +0300

    refactor: Implement signal import in AuthenticationConfig ready method

--- src/apps/authentication/apps.py
+++ src/apps/authentication/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.authentication'
+    
+    def ready(self):
+        """Import signals when app is ready"""
+        import apps.authentication.signals
